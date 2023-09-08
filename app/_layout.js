813f5e509c03ec9fca7cea087c64316d9be17e88
vikings2401 <robb.smith08@gmail.com>
2023-09-08 16:17:40 -0500

    deleting unnecessary files and react native tutorial progress

--- app/_layout.js
+++ app/_layout.js
@@ -1,6 +1,12 @@
 import { Stack } from 'expo-router';
+import { useCallback } from 'react';
+import {useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
+    
     return <Stack />;
 }
 
