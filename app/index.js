fff6cbf012f654633e3f73f522ed4dac271d900c
vikings2401 <robb.smith08@gmail.com>
2023-09-08 15:26:22 -0500

    adding files from tutorial

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 const Home = () => {
     return (
         <View>
-            <Text>L.L. Engineering to the moon!!!</Text>
+            <Text>Home</Text>
         </View>
     )
 }
