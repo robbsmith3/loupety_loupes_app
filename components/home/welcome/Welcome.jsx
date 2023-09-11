7971d0de9b0da2b31a6faa4525acd638a75169fe
Robb Smith III <robb.smith08@gmail.com>
2023-09-11 00:05:55 -0500

    rn tutorial prog

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -7,7 +7,7 @@ import {
   Image, 
   Flatlist
  } from 'react-native';
- import { useRouter } from 'expo-router';
+import { useRouter } from 'expo-router';
 import styles from './welcome.style';
 import { icons, SIZES } from '../../../constants';
 
@@ -15,6 +15,7 @@ const jobTypes = ["Full-time", "Part-time", "Contractor"]
 
 const Welcome = () => {
   const router = useRouter();
+  const [activeJobType, setActiveJobType] = useState('Full-time')
 
   return (
     <View>
@@ -46,10 +47,19 @@ const Welcome = () => {
         <Flatlist 
           data={jobTypes}
           renderItem={({ item }) => (
-            <TouchableOpacity>
-              <Text>{item}</Text>
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item);
+                router.push(`/search/${item}`)
+              }}
+            >
+              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
             </TouchableOpacity>
           )}
+          keyExtractor={item => item}
+          contentContainerStyle={{ columnGap: SIZES.small}}
+          horizontal
         />
       </View>
     </View>
